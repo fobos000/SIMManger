66e773b4f3bd850dd55aa0b4c3823731a1dc5407
Ostap.Horbach <Ostap-Horbach@admins-Mac-mini.local>
2015-08-19 13:53:33 +0300

    Initial Commit

---
+++ SIMTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SIMTest
+//
+//  Created by Ostap.Horbach on 8/19/15.
+//  Copyright (c) 2015 Ostap.Horbach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SIMTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SIMTest
+//
+//  Created by Ostap.Horbach on 8/19/15.
+//  Copyright (c) 2015 Ostap.Horbach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
